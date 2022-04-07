00512299e17ae3453d6981d6227149dc2a0e4e91
agh11 <agh1118@naver.com>
2022-04-08 01:02:07 +0900

    update test

--- RNN_tf.py
+++ RNN_tf.py
@@ -3,6 +3,9 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
 from tensorflow.keras.utils import to_categorical
 
+
+
+
 text="""경마장에 있는 말이 뛰고 있다\n
 그의 말이 법이다\n
 가는 말이 고와야 오는 말이 곱다\n"""
